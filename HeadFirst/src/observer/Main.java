285c18708396cf32969d91f12162ed498c2acc7e
gitTukasa <12752043infig@gmail.com>
2020-02-23 18:51:10 +0900

    comitTest

---
+++ HeadFirst/src/observer/Main.java
@@ -0,0 +1,25 @@
+package observer;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Observer Method Start");
+		// TODO 自動生成されたメソッド・スタブ
+		
+		Observer o = new ConcreteObserver();
+		Observer o1 = new ConcreteObserver();
+		Observer o2 = new ConcreteObserver();
+		
+		Subject sub = new ConcreteSubject()
+			.addObserver(o)
+			.addObserver(o1)
+			.addObserver(o2);
+		
+		((ConcreteSubject) sub).setString("settest");
+		
+//		System.err.println("End");
+		
+		
+
+	}
+}
